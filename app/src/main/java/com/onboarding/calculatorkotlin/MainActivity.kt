ea441ef2b3f95ea59703be45d75d1775960f1a22
Nicolas Laurenti <jose.laurenti@globant.com>
2021-01-15 11:39:44 -0300

    First Commit

---
+++ app/src/main/java/com/onboarding/calculatorkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.onboarding.calculatorkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
