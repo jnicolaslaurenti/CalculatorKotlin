0f62c97370d3e3cbba8481f4c83241fc87e1963a
Nicolas Laurenti <jose.laurenti@globant.com>
2021-01-18 10:51:36 -0300

    Solved comment

--- app/src/main/java/com/onboarding/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/onboarding/calculatorkotlin/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.onboarding.calculatorkotlin
 
-import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
