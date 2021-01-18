8a6571b100bdeb4115538adf28e2469ff851c49e
Nicolas Laurenti <jose.laurenti@globant.com>
2021-01-18 10:23:19 -0300

    Changed width and lenght, added icon and lock landscape

--- app/src/main/java/com/onboarding/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/onboarding/calculatorkotlin/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.onboarding.calculatorkotlin
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
